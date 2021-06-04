4bc2a5cc3de1a77cc45a8d4f14f5a3c6c175811b
smriti1909 <78542312+smriti1909@users.noreply.github.com>
2021-06-04 16:08:09 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        this.pointB= pointB;
+        World.add(world, this.chain);
+    }
+    fly(){
+        this.chain.bodyA= null;
+    }
+
+    display(){
+        if(this.chain.bodyA!==null){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
